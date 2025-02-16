6edb7dadd96d698d985920c522117b6abd0ae67d
Raul <rgarciat94@gmai.com>
2025-02-16 12:03:59 +0100

    Añadir url frontend

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
+require('dotenv').config();
 const express = require('express');
-const router = require('./routes');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const router = require('./routes');  
 
 const app = express();
 const port = process.env.PORT || 3000; 
 
-app.use(cors());                    // Habilitamos CORS
-app.use(bodyParser.json());         // Parseamos el body como JSON
+// Configurar CORS con la URL del frontend desde las variables de entorno
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+}));
+
+app.use(bodyParser.json());  // Parseamos el body como JSON
 
 // Usamos las rutas
-app.use('/api', router);            // Todas las rutas empezarán con /api
+app.use('/api', router);  // Todas las rutas empezarán con /api
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en puerto ${port}`);
